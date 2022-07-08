edfa9a49f7cc9e90fc8768f05938b34839aa2cea
prenwyn <wynpren@gmail.com>
2022-07-08 01:00:49 +0100

    added files

---
+++ README.md
@@ -0,0 +1,38 @@
+# copy_to_output
+A small rust library to copy files/folders from the project directory to the output directory  
+
+## Copy file/folder
+`copy_to_output("path name", "build profile");`
+
+## When building
+You can use a build.rs file to copy a file/folder to the output directory when running cargo build  
+
+**Example build.rs**  
+```rs
+ use std::env;  
+ use copy_to_output::copy_to_output;  
+   
+ fn main() {  
+     // Re-runs script if any files in res are changed  
+     println!("cargo:rerun-if-changed=res/*");  
+     copy_to_output("res", &env::var("PROFILE").unwrap()).expect("Could not copy");  
+ }
+ ``` 
+ where 'res' is the name of the directory to copy to the output directory  
+  
+**Example Cargo.toml**  
+```rs
+[package]
+name = "test_crate"
+version = "0.1.0"
+edition = "2021"
+
+# See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
+
+[build-dependencies]
+copy_to_output = "1.0"
+glob = "0.3""
+ ```
+ glob provides the * wildcard operator for the println!() line in build.rs  
+   
+ Both of these examples are in the example_files folder in this repo  
---
+++ src/lib.rs
@@ -0,0 +1,76 @@
+use fs_extra::copy_items;
+use fs_extra::dir::CopyOptions;
+use anyhow::*;
+
+pub fn copy_to_output(path: &str, build_type: &str) -> Result<()> {
+    let mut options = CopyOptions::new();
+    let mut from_path = Vec::new();
+    let out_path = format!("target/{}", build_type);
+
+    // Overwrite existing files with same name
+    options.overwrite = true;
+
+    from_path.push(path);
+    copy_items(&from_path, &out_path, &options)?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+    use std::fs::File;
+    use std::path::Path;
+    use crate::copy_to_output;
+
+    #[test]
+    fn test_file_copied() {
+        let file_name = "test_file";
+
+        // Create test file to copy
+        File::create(file_name).expect("could not create file");
+
+        // Copy test file to output
+        copy_to_output(file_name, "debug").expect("could not copy file");
+        assert_eq!(true, Path::new(&format!("target/debug/{}", file_name)).exists());
+
+        // Clean up test file
+        fs::remove_file(file_name).expect("failed to cleanup file");
+        fs::remove_file(format!("target/debug/{}", file_name)).expect("failed to cleanup file");
+    }
+
+    #[test]
+    fn test_empty_directory_copied() {
+        let dir_name = "empty_test_dir";
+
+        // Create test directory to copy
+        fs::create_dir(dir_name).expect("could not create directory");
+
+        // Copy test directory to output
+        copy_to_output(dir_name, "debug").expect("could not copy directory");
+        assert_eq!(true, Path::new(&format!("target/debug/{}", dir_name)).exists());
+
+        // Clean up test directory
+        fs::remove_dir(dir_name).expect("failed to cleanup directory");
+        fs::remove_dir(format!("target/debug/{}", dir_name)).expect("failed to cleanup directory");
+    }
+
+    #[test]
+    fn test_directory_with_subfile_copied() {
+        let dir_name = "test_dir";
+        let file_name = "test_subfile";
+
+        // Create test directory and test file to copy
+        fs::create_dir(dir_name).expect("could not create directory");
+        File::create(Path::new(&format!("{}/{}", dir_name, file_name))).expect("coud not create file");
+
+        // Copy test directory to output
+        copy_to_output(dir_name, "debug").expect("could not copy directory");
+        assert_eq!(true, Path::new(&format!("target/debug/{}", dir_name)).exists()
+                   && Path::new(&format!("target/debug/{}/{}", dir_name, file_name)).exists());
+
+        // Clean up test directory and test file
+        fs::remove_dir_all(dir_name).expect("failed to cleanup directory");
+        fs::remove_dir_all(format!("target/debug/{}", dir_name)).expect("failed to cleanup directory");
+    }
+}
